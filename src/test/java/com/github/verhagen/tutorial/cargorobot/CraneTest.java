29d0a1c93aa4ca2d800a1dc57e5400c6ba1665e9
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-10-10 21:50:26 +0200

    Adds TODOs and FIXMEs - the FIXMEs show the order in which to create the unit-tests

--- src/test/java/com/github/verhagen/tutorial/cargorobot/CraneTest.java
+++ src/test/java/com/github/verhagen/tutorial/cargorobot/CraneTest.java
@@ -13,37 +13,42 @@ public class CraneTest {
 		assertEquals(0, crane.getLocation());
 
 		crane.moveRight();
-		assertEquals(1, crane.getLocation());
+		// FIXME 1
+		// TODO Add assert for new position
+		fail("Implement the TODO items");
 	}
 
 	@Test
 	public void commandMoveRightOnMaxRight() throws Exception {
 		int railLength = 4;
 		Crane crane = new Crane(railLength, railLength -1);
-		assertEquals(3, crane.getLocation());
-
-		crane.moveRight();
-		assertEquals(3, crane.getLocation());
+		// FIXME 3
+		// TODO Add assert for position
+		// TODO Move the crane right
+		// TODO Add assert for position is the same
+		fail("Implement the TODO items");
 	}
 
 	@Test
 	public void commandMoveLeft() throws Exception {
 		int railLength = 4;
 		Crane crane = new Crane(railLength, railLength -1);
-		assertEquals(3, crane.getLocation());
-
-		crane.moveLeft();
-		assertEquals(2, crane.getLocation());
+		// FIXME 2
+		// TODO Add assert for position
+		// TODO Move Left
+		// TODO Add assert for new position
+		fail("Implement the TODO items");
 	}
 
 	@Test
 	public void commandMoveLeftOnMaxLeft() throws Exception {
 		int railLength = 4;
 		Crane crane = new Crane(railLength, 0);
-		assertEquals(0, crane.getLocation());
-
-		crane.moveLeft();
-		assertEquals(0, crane.getLocation());
+		// FIXME 4
+		// TODO Add assert for position
+		// TODO Move the crane left
+		// TODO Add assert for position is the same
+		fail("Implement the TODO items");
 	}
 
 }
